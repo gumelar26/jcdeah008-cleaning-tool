0850207b6d9eaf5efa129ea6a2cdd5dff8cb07fd
gumelar26 <gumelarmuhamad26@gmail.com>
2025-12-18 20:58:55 +0700

    changes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 df = pd.read_json('data.json')
+<<<<<<< HEAD
 # connection to database
 
 # add utils
+=======
+>>>>>>> parent of 2735e32... combine utils and database in main
